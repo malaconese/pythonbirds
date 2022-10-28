a57c8bb4626e550a2dba12dcb79d0e39a90dbca9
malaconês <victor.gabriel.ozorio@gmail.com>
2022-10-28 17:13:17 -0300

    Criados atributos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,15 @@
 class Pessoa:
+    def __init__(self, nome=None):
+        self.nome = nome
+
     def cumprimentar(self):
         return f'olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Luciano')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Renzo'
+    print(p.nome)
